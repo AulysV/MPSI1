ea0ee9a42831b8be724613eda633de30906c8d8e
Aulys VINAY <aulys.n.vinay@outlook.com>
2023-12-26 15:08:00 +0100

    Base

--- MPSI1/tailwind.config.js
+++ MPSI1/tailwind.config.js
@@ -6,9 +6,9 @@ export default {
   },
   plugins: [require("daisyui")],
 
-   daisyui: {
+  daisyui: {
     themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "sunset", // name of one of the included themes for dark mode
+    darkTheme: "emerald", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
