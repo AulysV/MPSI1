4c6b9b40008de59eb878b1180b87b8425073d23a
Aulys VINAY <aulys.n.vinay@outlook.com>
2023-12-27 13:23:30 +0100

    testimg

--- MPSI1/svelte.config.js
+++ MPSI1/svelte.config.js
@@ -1,6 +1,9 @@
 // import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import adapter from "@sveltejs/adapter-vercel";
+import { importAssets } from "svelte-preprocess-import-assets";
+
+svelte({ preprocess: [importAssets()] });
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,6 +12,7 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    preprocess: [importAssets()],
   },
   preprocess: vitePreprocess(),
 };
