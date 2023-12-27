ba12fe32dc85600106911873a3fd03f5fe969e8b
Aulys VINAY <aulys.n.vinay@outlook.com>
2023-12-27 13:25:07 +0100

    f

--- MPSI1/svelte.config.js
+++ MPSI1/svelte.config.js
@@ -1,9 +1,9 @@
 // import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import adapter from "@sveltejs/adapter-vercel";
-import { importAssets } from "svelte-preprocess-import-assets";
+// import { importAssets } from "svelte-preprocess-import-assets";
 
-svelte({ preprocess: [importAssets()] });
+// svelte({ preprocess: [importAssets()] });
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +12,7 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    preprocess: [importAssets()],
+    // preprocess: [importAssets()],
   },
   preprocess: vitePreprocess(),
 };
