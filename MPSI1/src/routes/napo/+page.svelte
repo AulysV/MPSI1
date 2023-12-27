a15988143ef9f63beb21ff4b289ef61d0eb0af72
Aulys VINAY <aulys.n.vinay@outlook.com>
2023-12-27 14:48:53 +0100

    Add Napoléours

---
+++ MPSI1/src/routes/napo/+page.svelte
@@ -0,0 +1,53 @@
+<div class="hero h-svh">
+  <h1 class="z-10 text-5xl text-neutral-content bg-base-300 p-5 rounded-2xl">Heil Napoléours !!</h1>
+  <div class="carousel">
+    <div id="slide1" class="carousel-item relative w-full">
+      <img
+        src="https://cdn.discordapp.com/attachments/971357234980327424/1189561209654083604/Sans_titre.jpg?ex=659e9c35&is=658c2735&hm=4637e16d0a1a308d93ebf430e8bd209c4097fc2bd31df49073cebd830791bb81&"
+        class="w-full max-h-svh object-cover object-top blur-sm brightness-50"
+      />
+      <div
+        class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+      >
+        <a href="#slide4" class="btn btn-circle">❮</a>
+        <a href="#slide2" class="btn btn-circle">❯</a>
+      </div>
+    </div>
+    <div id="slide2" class="carousel-item relative w-full">
+      <img
+        src="https://cdn.discordapp.com/attachments/971357234980327424/1189545367876948048/nap.jpg?ex=659e8d74&is=658c1874&hm=719f55e051ab64608491510b0e5d623fcae787238d3debb366541a5b59ccba22&"
+        class="w-full max-h-svh object-cover blur-sm brightness-50"
+      />
+      <div
+        class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+      >
+        <a href="#slide1" class="btn btn-circle">❮</a>
+        <a href="#slide3" class="btn btn-circle">❯</a>
+      </div>
+    </div>
+    <div id="slide3" class="carousel-item relative w-full">
+      <img
+        src="https://cdn.discordapp.com/attachments/971357234980327424/1189561191983497236/WhatsApp_Image_2023-12-21_at_20.14.12.jpeg?ex=659e9c31&is=658c2731&hm=c8121d08cc1ee37b14a9c7337e9e4ba0fb1a67495e1dca6ad147b62dbce9e6a4&"
+        class="w-full max-h-svh object-cover object-top blur-sm brightness-50"
+      />
+      <div
+        class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+      >
+        <a href="#slide2" class="btn btn-circle">❮</a>
+        <a href="#slide4" class="btn btn-circle">❯</a>
+      </div>
+    </div>
+    <div id="slide4" class="carousel-item relative w-full">
+      <img
+        src="https://cdn.discordapp.com/attachments/971357234980327424/1189561192327413840/WhatsApp_Image_2023-12-21_at_21.45.39.jpeg?ex=659e9c31&is=658c2731&hm=b0d161e62e6c88d3549c90af877a747fa2ac37d6cbed220e043535b3916aa192&"
+        class="w-full max-h-svh object-cover blur-sm brightness-50"
+      />
+      <div
+        class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+      >
+        <a href="#slide3" class="btn btn-circle">❮</a>
+        <a href="#slide1" class="btn btn-circle">❯</a>
+      </div>
+    </div>
+  </div>
+</div>
