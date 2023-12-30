b53f0ca60b354fa5db88f20536f5fd3dc0923ab9
Aulys VINAY <aulys.n.vinay@outlook.com>
2023-12-30 13:27:19 +0100

    Napoléours

--- MPSI1/src/routes/napo/+page.svelte
+++ MPSI1/src/routes/napo/+page.svelte
@@ -1,3 +1,27 @@
-<div class="hero h-svh">
-  
+<div class="h-100 px-10 py-20 lg:px-20">
+  <h1 class="text-3xl py-10 lg:text-5xl">Napoléours.</h1>
+  <div id="1" class="flex">
+    <p class="py-3 text-xl lg:text-3xl text-justify">
+      Dans les interminables couloirs du lycée Albert Schweitzer, émanait une
+      aura singulière, une présence silencieuse qui transcendait et illuminait
+      la morosité des jours d’hiver.
+    </p>
+  </div>
+
+  <p class="py-3 text-xl lg:text-3xl text-justify">
+    Napoléours, telle était la dénomination de cette personnification, ou
+    devrait-on dire, de l’oursification de l’Excellence et de la Gloire.
+  </p>
+  <p class="py-3 text-xl lg:text-3xl text-justify">
+    Cette lueur a été octroyée, ni plus ni moins, que par le magnanime Napoléon
+    Bonaparte, éminence des mathématiques qui régissait avec une main de maître
+    les destinées de la MPSI1.
+  </p>
+  <p class="py-3 text-xl lg:text-3xl text-justify">
+    L’origine de la rutilance qu’est Napoléours, est quant à elle presque aussi
+    énigmatique que la notation des professeurs de philosophie. Presque, car des
+    fragments de légendes sont égrenées au fil des cours de mathématiques, et
+    les élèves les plus attentifs auront ainsi la chance de pouvoir espérer
+    reconstituer le passé encore nébuleux du grand Napoléours.
+  </p>
 </div>
