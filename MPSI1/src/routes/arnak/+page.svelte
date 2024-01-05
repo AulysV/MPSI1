a21b9d7f1d9568dd326ffac3d3b1e627fabfaeb2
Aulys VINAY <aulys.n.vinay@outlook.com>
2024-01-05 13:25:44 +0100

    Add arnak

---
+++ MPSI1/src/routes/arnak/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+  let motdepasseentré = "";
+  const vraimotdepasse = "monkey";
+
+  function verif() {
+    if (motdepasseentré === vraimotdepasse) {
+      const messageDiv = document.getElementById("message");
+      messageDiv.textContent = "Voici une petite musique ";
+
+      setTimeout(() => {
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+      }, 5000);
+    } else {
+      alert("Accès refusé!");
+    }
+  }
+</script>
+
+<div class="h-svh text-center flex flex-col justify-center items-center">
+  <h1 class="text-3xl text-center font-semibold my-5">
+    Veuillez entrer le mot de passe :
+  </h1>
+
+  <input
+    type="text"
+    class="input input-bordered w-full max-w-xs my-5"
+    bind:value={motdepasseentré}
+    placeholder="Mot de passe"
+  />
+  <button class="btn btn-sm sm:btn-sm md:btn-md my-5" on:click={verif}
+    >Vérifier</button
+  >
+
+  <div id="message"></div>
+</div>
